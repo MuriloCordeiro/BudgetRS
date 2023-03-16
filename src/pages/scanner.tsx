b0d6da4671689734a929de1d5b9c73abf8298803
Murilo <murilo.ramos@rspneus.com.br>
2023-03-16 11:09:35 -0300

    chore: testing

--- src/pages/scanner.tsx
+++ src/pages/scanner.tsx
@@ -32,25 +32,19 @@ import {
 import ReactToPrint from "react-to-print";
 // import myFunction from "../utils";
 export default function Scanner() {
-  const [barcodeScan, setBarcodeScan] = useState<any>("");
-  const [verifyTest, setVerifyTest] = useState<Boolean>();
-  const [test, setTest] = useState<any>();
-  const [errorCode, setErrorCode] = useState();
+  const [barcodeScan, setBarcodeScan] = useState<any>(
+    "Nenhum código de barra escaneado"
+  );
+  const [verifyScanner, setVerifyScanner] = useState<Boolean>();
+  const [errorMessage, setErrorMessage] = useState<any>();
 
-  // function testing() {
-  //   myFunction();
-  // }
-
-  const { isOpen, onOpen, onClose } = useDisclosure();
-
-  const componentRef = useRef<any>();
-  const Mock = [
+  const [mock, setMock] = useState([
     {
       itens: "185/65R15 88H OUTRUN M2 MOMO",
       end1: "1A-2D",
       end2: "3A-A",
       end3: "3A-A",
-      EAN: "1189189600024300092459400010004",
+      EAN: "1189189600024300092462600020004",
       QTD: 5,
       CONFERIDO: 0,
     },
@@ -59,7 +53,7 @@ export default function Scanner() {
       end1: "1A-2D",
       end2: "1A-2D",
       end3: "1A-2D",
-      EAN: "1189189600024300092429600040004",
+      EAN: "1189189600024300092508900020002",
       QTD: 10,
       CONFERIDO: 0,
     },
@@ -68,126 +62,185 @@ export default function Scanner() {
       end1: "1A-2D",
       end2: "1A-2D",
       end3: "1A-2D",
-      EAN: "1189189600024300092429600040004",
+      EAN: "1189189600024300092508900020002",
       QTD: 10,
       CONFERIDO: 0,
     },
-  ];
+  ]);
 
-  const toast = useToast();
+  const toast = useToast({
+    duration: 1500,
+    isClosable: true,
+    containerStyle: {
+      color: "white",
+    },
+  });
+
+  const { isOpen, onOpen, onClose } = useDisclosure();
+
+  const componentRef = useRef<any>();
+
+  function encontraDadoParecido() {
+    const checkEAN = (Mock: any) => Mock.EAN === barcodeScan;
+
+    const result = mock.some(checkEAN);
+
+    if (verifyScanner === true && result === true) {
+      const mappedMock = mock?.map((prod) => {
+        if (prod?.EAN === barcodeScan) {
+          setBarcodeScan(null);
+          if (prod?.EAN === barcodeScan && prod?.CONFERIDO + 1 > prod?.QTD) {
+            console.log("deu errado pq o conferido ta maior");
+            setVerifyScanner(false);
+            fetch("https://localhost:5001/ExpeditionScannerAPI");
+            return prod;
+          } else {
+            return { ...prod, CONFERIDO: prod.CONFERIDO + 1 };
+          }
+        } else {
+          setBarcodeScan(null);
+          return prod;
+        }
+      });
+      setMock(mappedMock);
+    } else if (verifyScanner === true) {
+      setBarcodeScan(null);
+      fetch("https://localhost:5001/ExpeditionScannerAPI");
+    }
+  }
+
+  useEffect(() => {
+    if (barcodeScan !== null) {
+      encontraDadoParecido();
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [barcodeScan]);
+
+  // const mappedMock = mock?.map((prod)=>{
+  //   if (prod?.EAN === barcodeScan){
+  //     return { ...prod, CONFERIDO: prod.CONFERIDO + 1 };
+  //   } else {
+  //       return prod;
+  //   }
+  // })
+
+  //     const mappedMock = mock?.map((prod) => {
+
+  //       if (prod?.EAN === barcodeScan) {
+  //       } else {
+  //           if (prod?.CONFERIDO > prod?.QTD) {
+  //             // fetch("https://localhost:5001/ExpeditionScannerAPI");
+  //             console.log(
+  //               `Item: ${prod?.EAN}, CONFERIDO: ${prod?.CONFERIDO + 1} de ${
+  //                 prod?.QTD
+  //               }`
+  //             );
+  //           return { ...prod, CONFERIDO: prod.CONFERIDO + 1 };
+  //         }
+  //       } else if (verifyScanner === true && prod?.EAN !== barcodeScan) {
+  //         console.log("QTD do item máximo");
+  //         fetch("https://localhost:5001/ExpeditionScannerAPI");
+
+  //         setIsOpenError(true);
+  //         setErrorMessage("QTD do item máximo");
+  //         handlePlayError();
+  //       }
+  //       return prod;
+  //     });
+  //   } else if (verifyScanner === true && result === false) {
+  //     fetch("https://localhost:5001/ExpeditionScannerAPI");
+  //     console.log("EAN não existe", result);
+  //   }
+  // }
+
+  // useEffect(() => {
+  //   encontraDadoParecido();
+  // }, [barcodeScan]);
 
   if (typeof window !== "undefined") {
     // eslint-disable-next-line react-hooks/rules-of-hooks
     useScanDetection({
       onComplete: (barcode): any => {
-        {
-          verifyTest === true
-            ? setBarcodeScan(barcode)
-            : setBarcodeScan("Nenhum item escaneado");
+        if (verifyScanner === true) {
+          setBarcodeScan(barcode.trim());
+        } else {
+          setBarcodeScan("Nenhum código válido escaneado");
         }
+        // encontraDadoParecido();
+        // ? : setBarcodeScan("");
       },
       // onError: setErrorCode,
       preventDefault: false,
-      minLength: 14,
+      minLength: 13,
       stopPropagation: true,
     });
-
-    console.log("Esse é o código de barras", barcodeScan);
-    console.log("Esse é o código de error", errorCode);
   }
 
-  // const handleButtonClick = () => {
-  //   const event = new KeyboardEvent("keydown", {
-  //     ctrlKey: true,
-  //     shiftKey: true,
-  //     code: "80",
-  //   });
+  useEffect(() => {
+    if (verifyScanner === true) {
+      toast({
+        title: "Verificação Iniciada.",
+        description: "O scanner foi habilitado.",
+        status: "success",
+      });
+    } else if (verifyScanner === false) {
+      toast({
+        title: "Verificação Finalizada.",
+        description: "O scanner foi desabilitado.",
+        status: "error",
+      });
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [verifyScanner]);
 
-  //   window.dispatchEvent(event);
-
-  //   console.log("event", event);
-  // };
   // useEffect(() => {
-  //   if (verifyTest === true) {
-  //     toast({
-  //       title: "Verificação Iniciada.",
-  //       description: "Você já pode usar o Scanner.",
-  //       status: "success",
-  //       duration: 9000,
-  //       isClosable: true,
-  //     });
-  //   } else {
-  //     toast({
-  //       title: "Verificação Encerrada.",
-  //       description: "O scanner foi desabilitado.",
-  //       status: "error",
-  //       duration: 9000,
-  //       isClosable: true,
-  //     });
+  //   if (barcodeScan.trim(" ") !== "1189189600024300092462600020004") {
+  //     fetch("https://localhost:5001/ExpeditionScannerAPI");
   //   }
-  // }, [toast, verifyTest]);
-
-  function fetchScanner() {
-    fetch("https://localhost:5001/ExpeditionScannerAPI");
-  }
+  // }, [barcodeScan]);
+  // function fetchScanner() {}
   return (
     <>
       {/* <Flex p="2rem">teste:{barcodeScan}</Flex>
       <Button mt="10rem">teste</Button> */}
 
-      <Button onClick={() => fetchScanner()}>Olá</Button>
+      {/* <Button onClick={() => fetchScanner()}>Olá</Button> */}
       <Flex p="2rem" direction="column">
         <Flex>Esse é o código correto: {barcodeScan}</Flex>
         <Flex w="80%" mt="5rem" justify="space-evenly">
-          <Input
-            mr="1rem"
-            placeholder="Número do pedido"
-            onChange={(e) => {
-              setTest(e.target.value);
-            }}
-            value={test}
-          />
-          {/* <Button onClick={testing}>teste</Button> */}
+          <Input mr="1rem" placeholder="Número do pedido" />
+          {/* <Button onClick={encontraDadoParecido}>Comparar</Button> */}
 
           <Input mr="1rem" placeholder="Separador" />
           <Input mr="1rem" placeholder="Conferente" />
 
           <Button
+            isDisabled={verifyScanner ? true : false}
             mr="1rem"
             w="full"
             bgColor={"#005F27"}
             color="white"
-            onClick={() => {
-              setVerifyTest(true);
+            _hover={{
+              bgColor: "#083b19",
             }}
-            onClickCapture={() => {
-              toast({
-                title: "Verificação Iniciada.",
-                description: "Você já pode usar o Scanner.",
-                status: "success",
-                duration: 9000,
-                isClosable: true,
-              });
+            onClick={() => {
+              setVerifyScanner(true);
             }}
           >
-            Iniciar conferencia
+            Iniciar conferência
           </Button>
 
           <Button
+            isDisabled={!verifyScanner ? true : false}
             w="full"
             bgColor={"red"}
             color="white"
-            onClick={() => {
-              setVerifyTest(false);
+            _hover={{
+              bgColor: "#b40505",
             }}
-            onClickCapture={() => {
-              toast({
-                title: "Verificação Encerrada.",
-                description: "O scanner foi desabilitado.",
-                status: "error",
-                duration: 9000,
-                isClosable: true,
-              });
+            onClick={() => {
+              setVerifyScanner(false);
+              setBarcodeScan("Nenhum código de barra escaneado");
             }}
           >
             Cancelar conferencia
@@ -211,7 +264,7 @@ export default function Scanner() {
             </Thead>
 
             <Tbody>
-              {Mock.map((prod, index) => (
+              {mock.map((prod, index) => (
                 <Tr key={index}>
                   <Td>{prod.itens}</Td>
                   <Td>{prod.end1}</Td>
