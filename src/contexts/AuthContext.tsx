da495ebed0a348c98c26c8cb665f76111edc20fc
alexsandro cubas <alexsandro.cubas@pneufree.com.br>
2023-04-24 11:27:13 -0300

    Feat: toastlogin

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -1,10 +1,10 @@
 // import { AuthProvider } from "firebase/auth";
 import {
-  createContext,
-  useState,
-  useEffect,
-  ReactNode,
-  useContext,
+    createContext,
+    useState,
+    useEffect,
+    ReactNode,
+    useContext,
 } from "react";
 
 import { User } from "firebase/auth";
@@ -16,71 +16,79 @@ import { ValidateAuth } from "../hooks/post/postValidateAuth";
 import { useRouter } from "next/router";
 
 type AuthContextData = {
-  signInEmailPasswordWebservices: any;
-  isLoading: boolean | undefined;
+    signInEmailPasswordWebservices: any;
+    isLoading: boolean | undefined;
+    isLoged: boolean | null;
+    setIsLoged: any;
 };
 
 type AuthProviderProps = {
-  children: ReactNode;
+    children: ReactNode;
 };
 
 const AuthContext = createContext({} as AuthContextData);
 
 export function AuthProvider({ children }: AuthProviderProps) {
-  const Router = useRouter();
-  const [isLoading, setIsLoading] = useState<boolean>();
-  const CLIENT_TOKEN: any = process.env.NEXT_PUBLIC_CLIENT_TOKEN;
-  const COOKIE_MAX_AGE: any = process.env.NEXT_PUBLIC_CLIENT_MAX_AGE;
-  const cookies = parseCookies();
-  const userToken = cookies[CLIENT_TOKEN];
+    const Router = useRouter();
+    const [isLoading, setIsLoading] = useState<boolean>();
+    const [isLoged, setIsLoged] = useState<boolean | null>(null);
+    const CLIENT_TOKEN: any = process.env.NEXT_PUBLIC_CLIENT_TOKEN;
+    const COOKIE_MAX_AGE: any = process.env.NEXT_PUBLIC_CLIENT_MAX_AGE;
+    const cookies = parseCookies();
+    const userToken = cookies[CLIENT_TOKEN];
 
-  async function signInEmailPasswordWebservices(
-    email: string,
-    password: string
-  ): Promise<void> {
-    try {
-      setIsLoading(true);
-      const response = await ValidateAuth(email, password);
-      const isAuthenticated = response && response.data.authenticated;
-      await handleSessionCookie(isAuthenticated);
-      setIsLoading(false);
-    } catch {
-      console.error("error");
+    async function signInEmailPasswordWebservices(
+        email: string,
+        password: string
+    ): Promise<void> {
+        try {
+            setIsLoading(true);
+            const response = await ValidateAuth(email, password);
+            setIsLoged(response?.data?.authenticated);
+            const isAuthenticated = response && response.data.authenticated;
+            await handleSessionCookie(isAuthenticated);
+            setIsLoading(false);
+        } catch {
+            console.error("error");
+        }
     }
-  }
 
-  function handleSessionCookie(isAuthenticated: boolean) {
-    if (isAuthenticated === true) {
-      setCookie(undefined, CLIENT_TOKEN, isAuthenticated.toString(), {
-        maxAge: COOKIE_MAX_AGE,
-        path: "/",
-      });
+    function handleSessionCookie(isAuthenticated: boolean) {
+        if (isAuthenticated === true) {
+            setCookie(undefined, CLIENT_TOKEN, isAuthenticated.toString(), {
+                maxAge: COOKIE_MAX_AGE,
+                path: "/",
+            });
 
-      Router.push("/homepage");
-    } else {
-      destroyCookie(userToken as any, CLIENT_TOKEN);
-      Router.push("/");
+            Router.push("/homepage");
+        } else {
+            destroyCookie(userToken as any, CLIENT_TOKEN);
+            Router.push("/");
+        }
     }
-  }
 
-  useEffect(() => {
-    userToken ? console.log("salvou o cookie", userToken) : Router.push("/");
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [userToken]);
+    useEffect(() => {
+        userToken
+            ? console.log("salvou o cookie", userToken)
+            : Router.push("/");
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [userToken]);
 
-  return (
-    <AuthContext.Provider
-      value={{
-        signInEmailPasswordWebservices,
-        isLoading,
-      }}
-    >
-      {children}
-    </AuthContext.Provider>
-  );
+    return (
+        <AuthContext.Provider
+            value={{
+                signInEmailPasswordWebservices,
+                isLoading,
+                isLoged,
+                setIsLoged,
+            }}
+        >
+            {children}
+        </AuthContext.Provider>
+    );
 }
 export function useAuth(): AuthContextData {
-  const context = useContext(AuthContext);
+    const context = useContext(AuthContext);
 
-  return context;
+    return context;
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,199 +1,211 @@
 import {
-  Flex,
-  Button,
-  Text,
-  useBreakpointValue,
-  Input,
-  InputGroup,
-  InputLeftAddon,
-  InputLeftElement,
-  InputRightElement,
-  Stack,
-  Image,
-  useToast,
-  useDisclosure,
-  Toast,
-  IconButton,
-  Icon,
-  Box,
-  Img,
+    Flex,
+    Button,
+    Text,
+    Input,
+    InputLeftElement,
+    Image,
+    useToast,
 } from "@chakra-ui/react";
 import LayoutDesk from "../components/Layouts/layoutDesktop";
-// import Lottie from "react-lottie";
 import animationData from "../animations/login.json";
 import { useAuth } from "../contexts/AuthContext";
 import { AiOutlineUser, AiOutlineEyeInvisible } from "react-icons/ai";
 import { BsShieldLock } from "react-icons/bs";
-import { FcGoogle } from "react-icons/fc";
-import {
-  signInWithPopup,
-  GoogleAuthProvider,
-  createUserWithEmailAndPassword,
-  User,
-} from "firebase/auth";
-import { useState } from "react";
+
+import { useEffect, useState } from "react";
 
 import { destroyCookie, parseCookies } from "nookies";
-import Router, { useRouter } from "next/router";
 import {
-  MotionFlex,
-  animationFlex,
-  itemAnimation,
-  InputMotion,
-  inputAnimation,
+    MotionFlex,
+    itemAnimation,
+    InputMotion,
+    inputAnimation,
 } from "../styles/animation";
 export default function HomeLogin() {
-  const toast = useToast({
-    duration: 5000,
-    isClosable: true,
-  });
-  const [email, setEmail] = useState<string>("");
-  const [password, setPassword] = useState<string>("");
+    const toast = useToast({
+        duration: 5000,
+        isClosable: true,
+    });
+    const [email, setEmail] = useState<string>("");
+    const [password, setPassword] = useState<string>("");
 
-  const defaultOptions = {
-    loop: true,
-    autoplay: true,
-    animationData: animationData,
-    rendererSettings: {
-      preserveAspectRatio: "xMidYMid slice",
-    },
-  };
-  const { signInEmailPasswordWebservices, isLoading } = useAuth();
+    const defaultOptions = {
+        loop: true,
+        autoplay: true,
+        animationData: animationData,
+        rendererSettings: {
+            preserveAspectRatio: "xMidYMid slice",
+        },
+    };
+    const { signInEmailPasswordWebservices, isLoading, isLoged, setIsLoged } =
+        useAuth();
 
-  const Toast = useToast({
-    position: "bottom",
-    isClosable: true,
-    duration: 2500,
-    containerStyle: {
-      color: "white",
-    },
-  });
+    const Toast = useToast({
+        position: "bottom",
+        isClosable: true,
+        duration: 2500,
+        containerStyle: {
+            color: "white",
+        },
+    });
 
-  function handleLogin() {
-    signInEmailPasswordWebservices(email, password);
-  }
+    function handleLogin() {
+        signInEmailPasswordWebservices(email, password);
+    }
 
-  return (
-    <>
-      <Flex
-        bgColor="#EBEBEB"
-        w="full"
-        h="100vh"
-        align="center"
-        justify="center"
-      >
-        <MotionFlex
-          boxShadow="lg"
-          rounded="lg"
-          initial="hidden"
-          animate="visible"
-          variants={itemAnimation}
-          borderRadius="15px"
-          direction="column"
-          align="center"
-          justify="center"
-          px="3rem"
-          py="1rem"
-          bgColor="#FFFFFF"
-          w="35%"
-        >
-          <MotionFlex
-            justify="start"
-            direction="column"
-            variants={itemAnimation}
-            initial="hidden"
-            animate="visible"
-            align="center"
-            w="full"
-          >
-            <Flex justify="center">
-              <Image alt="Logo da RS" src={"/Image/RS-icon.svg"} w="120x" />
-              <Text fontSize={"70px"} fontWeight="Bold">
-                SE
-              </Text>
-              <Text fontSize={"70px"} textColor={"#E30613"} fontWeight="Bold">
-                RS
-              </Text>
-            </Flex>
-            <Text fontSize="18px" fontWeight="bold" align="center" mb="1rem">
-              Seja bem-vindo
-            </Text>
-            <Text
-              opacity="45%"
-              fontSize="14px"
-              color="black"
-              fontWeight="medium"
-              alignSelf="center"
+    useEffect(() => {
+        if (isLoged && isLoged !== null) {
+            Toast.closeAll();
+            Toast({
+                title: "Logado com sucesso",
+                status: "success",
+            });
+            setIsLoged(null);
+        } else if (!isLoged && isLoged !== null) {
+            Toast.closeAll();
+            Toast({
+                title: "Email ou senha incorretas",
+                status: "error",
+            });
+            setIsLoged(null);
+        }
+    }, [isLoged]);
+
+    return (
+        <>
+            <Flex
+                bgColor="#EBEBEB"
+                w="full"
+                h="100vh"
+                align="center"
+                justify="center"
             >
-              Faça o login para começar.
-            </Text>
-          </MotionFlex>
-          <InputMotion
-            variants={inputAnimation}
-            initial="hidden"
-            animate="visible"
-            mt="1rem"
-            w="full"
-            variant="solid"
-            size="md"
-          >
-            <InputLeftElement>
-              <AiOutlineUser opacity="45%" />
-            </InputLeftElement>
-            <Input
-              opacity="70%"
-              borderColor="gray.300"
-              variant="outline"
-              borderRadius="15px"
-              placeholder="Digite seu usuário do senior."
-              value={email}
-              onChange={(e) => {
-                setEmail(e.target.value);
-              }}
-            />
-          </InputMotion>
-          <InputMotion
-            variants={inputAnimation}
-            initial="hidden"
-            animate="visible"
-            w="full"
-            variant="solid"
-            size="md"
-            mt="1rem"
-          >
-            <InputLeftElement>
-              <BsShieldLock opacity="45%" />
-            </InputLeftElement>
-            <Input
-              type="password"
-              opacity="70%"
-              borderColor="gray.300"
-              variant="outline"
-              borderRadius="15px"
-              placeholder="Digite sua senha. "
-              value={password}
-              onChange={(e) => {
-                setPassword(e.target.value);
-              }}
-            />
-          </InputMotion>
+                <MotionFlex
+                    boxShadow="lg"
+                    rounded="lg"
+                    initial="hidden"
+                    animate="visible"
+                    variants={itemAnimation}
+                    borderRadius="15px"
+                    direction="column"
+                    align="center"
+                    justify="center"
+                    px="3rem"
+                    py="1rem"
+                    bgColor="#FFFFFF"
+                    w="35%"
+                >
+                    <MotionFlex
+                        justify="start"
+                        direction="column"
+                        variants={itemAnimation}
+                        initial="hidden"
+                        animate="visible"
+                        align="center"
+                        w="full"
+                    >
+                        <Flex justify="center">
+                            <Image
+                                alt="Logo da RS"
+                                src={"/Image/RS-icon.svg"}
+                                w="120x"
+                            />
+                            <Text fontSize={"70px"} fontWeight="Bold">
+                                SE
+                            </Text>
+                            <Text
+                                fontSize={"70px"}
+                                textColor={"#E30613"}
+                                fontWeight="Bold"
+                            >
+                                RS
+                            </Text>
+                        </Flex>
+                        <Text
+                            fontSize="18px"
+                            fontWeight="bold"
+                            align="center"
+                            mb="1rem"
+                        >
+                            Seja bem-vindo
+                        </Text>
+                        <Text
+                            opacity="45%"
+                            fontSize="14px"
+                            color="black"
+                            fontWeight="medium"
+                            alignSelf="center"
+                        >
+                            Faça o login para começar.
+                        </Text>
+                    </MotionFlex>
+                    <InputMotion
+                        variants={inputAnimation}
+                        initial="hidden"
+                        animate="visible"
+                        mt="1rem"
+                        w="full"
+                        variant="solid"
+                        size="md"
+                    >
+                        <InputLeftElement>
+                            <AiOutlineUser opacity="45%" />
+                        </InputLeftElement>
+                        <Input
+                            opacity="70%"
+                            borderColor="gray.300"
+                            variant="outline"
+                            borderRadius="15px"
+                            placeholder="Digite seu usuário do senior."
+                            value={email}
+                            onChange={(e) => {
+                                setEmail(e.target.value);
+                            }}
+                        />
+                    </InputMotion>
+                    <InputMotion
+                        variants={inputAnimation}
+                        initial="hidden"
+                        animate="visible"
+                        w="full"
+                        variant="solid"
+                        size="md"
+                        mt="1rem"
+                    >
+                        <InputLeftElement>
+                            <BsShieldLock opacity="45%" />
+                        </InputLeftElement>
+                        <Input
+                            type="password"
+                            opacity="70%"
+                            borderColor="gray.300"
+                            variant="outline"
+                            borderRadius="15px"
+                            placeholder="Digite sua senha. "
+                            value={password}
+                            onChange={(e) => {
+                                setPassword(e.target.value);
+                            }}
+                        />
+                    </InputMotion>
 
-          <Button
-            w="10rem"
-            boxShadow="lg"
-            isLoading={isLoading}
-            borderWidth="1px"
-            mt="2rem"
-            h="40px"
-            colorScheme="green"
-            onClick={() => {
-              handleLogin();
-            }}
-          >
-            Entrar
-          </Button>
-        </MotionFlex>
-      </Flex>
-    </>
-  );
+                    <Button
+                        w="10rem"
+                        boxShadow="lg"
+                        isLoading={isLoading}
+                        borderWidth="1px"
+                        mt="2rem"
+                        h="40px"
+                        colorScheme="green"
+                        onClick={() => {
+                            handleLogin();
+                        }}
+                    >
+                        Entrar
+                    </Button>
+                </MotionFlex>
+            </Flex>
+        </>
+    );
 }
